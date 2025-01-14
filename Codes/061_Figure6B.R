58dcca306885a5d5c49c74af01df6a3e9908b7b4
xkcococo <xkcoco95@gmail.com>
2025-01-14 17:08:24 -0600

    edits codes for figure 4 and uploaded codes for figure 6B

--- Codes/061_Figure6B.R
+++ Codes/061_Figure6B.R
@@ -1,4 +1,4 @@
-#### Figure 4 ####
+#### Figure 6B ####
 #### Author: Ke Xu, Hakmook Kang####
 
 # Load package 
@@ -16,8 +16,6 @@ library(readxl)
 
 ################ Import Data ################
 hipp<-read_excel("/Users/kexu/Library/CloudStorage/OneDrive-VUMC/Research/Active/20241022_HIPP/DATA0/20240411_HIPP_data_freeze_202211071_new_morphology_data_updated.xlsx", sheet="Master_table")
-
-
 
 ################ Clean data ################
 
@@ -67,25 +65,25 @@ hipp$center<-factor(hipp$center, levels = 1:5, labels = c("Scharp-Lacy","SC-ICRC
 hipp$center<-factor(hipp$center)
 hipp$Center<-factor(hipp$Center)
 
+hipp$BetaCellPct<-probitlink(hipp$Beta_Cells/100)
+hipp$AlphaCellPct<-probitlink(hipp$Alpha_Cells/100)
+hipp$DeltaCellPct<-probitlink(hipp$Delta_Cells/100)
+
 
 #####################################################################
 
 # Define the independent variables
-independent_vars_full <- "race2 + Donor_HbA1c + Age_years + Gender + center + BMI + Pre_shipment_Culture_Time_hours + Islet_Transit_Time_hours"
-independent_vars_reduced <- "Donor_HbA1c + Age_years + Gender + center + BMI + Pre_shipment_Culture_Time_hours + Islet_Transit_Time_hours"
+independent_vars_full <- "Gender + race2 + Donor_HbA1c + Age_years + center + BMI + Pre_shipment_Culture_Time_hours + Islet_Transit_Time_hours"
+independent_vars_reduced <- "race2 + Donor_HbA1c + Age_years + center + BMI + Pre_shipment_Culture_Time_hours + Islet_Transit_Time_hours"
 
+
+# List of dependent variables
+dependent_vars <- c(
+  "AlphaCellPct", "BetaCellPct", "DeltaCellPct"
+)
 
 
 #####################################################################
-
-### Insulin secretion traits
-# List of dependent variables
-dependent_vars <- c(
-  "INS_basal_ng_IEQ", "INS_1st_AUC_ng_IEQ", "INS_2nd_AUC_ng_IEQ", 
-  "INS_G_16_7_AUC_ng_IEQ", "INS_G_16_7_SI", "INS_G_16_7_IBMX_100_AUC_ng_IEQ", 
-  "INS_G_16_7_IBMX_100_SI", "INS_G_1_7_Epi_1_AUC_ng_IEQ", "INS_G_1_7_Epi_1_II_updated", 
-  "INS_KCl_20_AUC_ng_IEQ", "INS_KCl_20_SI"
-)
 
 # Initialize a list to store ANOVA results
 anova_results <- list()
@@ -102,20 +100,21 @@ coeff_p_values <- c()
 
 # Loop through dependent variables
 for (var in dependent_vars) {
+#  var = 'BetaCellPct'
   # Fit the full model
   formula_full <- as.formula(paste(var, "~", independent_vars_full))
   model_full <- lm(formula_full, data = hipp)
   
-  # Extract coefficients and p-values for race2
-  race_coeff <- summary(model_full)$coefficients[grep("race2", rownames(summary(model_full)$coefficients)), ]
-  coeffs <- race_coeff[, "Estimate"]
-  p_vals <- round(race_coeff[, "Pr(>|t|)"], 4)
-
+  # Extract coefficients and p-values for Gender
+  gender_coeff <- summary(model_full)$coefficients[grep("Gender", rownames(summary(model_full)$coefficients)), ]
+  coeffs <- gender_coeff["Estimate"]
+  p_vals <- round(gender_coeff["Pr(>|t|)"], 4)
+  
   # Perform ANOVA
   formula_reduced <- as.formula(paste(var, "~", independent_vars_reduced))
   model_reduced <- lm(formula_reduced, data = hipp)
   global_p <- anova(model_reduced, model_full)[2, "Pr(>F)"]
-
+  
   # Collect the global p-value and coefficient p-values
   global_p_values <- c(global_p_values, global_p)
   coeff_p_values <- c(coeff_p_values, p_vals)
@@ -123,7 +122,7 @@ for (var in dependent_vars) {
   # Add results for each race comparison
   for (i in 1:length(coeffs)) {
     results <- rbind(results, data.frame(
-      names = paste(var, rownames(race_coeff)[i], sep = " "),
+      names = paste(var, "Gender-Fem:Male", rownames(gender_coeff)[i], sep = " "),
       Coefficient = coeffs[i],
       P.value = p_vals[i],
       Adj.P.value = NA  # Placeholder for later
@@ -156,91 +155,3 @@ results$Adj.P.value[global_index] <- global_p_values_adj
 
 results
 
-
-
-
-#####################################################################
-
-
-### Glucagon secretion traits
-# List of dependent variables
-dependent_vars <- c(
-  "GCG_basal_pg_IEQ",
-  "GCG_G_16_7_AUC_pg_IEQ",
-  "GCG_G_16_7_II",
-  "GCG_G_16_7_IBMX_100_AUC_pg_IEQ",
-  "GCG_G_16_7_IBMX_100_SI",
-  "GCG_G_1_7_Epi_1_AUC_pg_IEQ",
-  "GCG_G_1_7_Epi_1_SI",
-  "GCG_KCl_20_AUC_pg_IEQ",
-  "GCG_KCl_20_SI"
-)
-
-# Initialize a list to store ANOVA results
-anova_results <- list()
-
-# Initialize a data frame to store the results
-results <- data.frame(
-  names = character(), Coefficient = numeric(), P.value = numeric(),
-  Adj.P.value = numeric(), stringsAsFactors = FALSE
-)
-
-# Initialize separate lists for global p-values and coefficient p-values
-global_p_values <- c()
-coeff_p_values <- c()
-
-# Loop through dependent variables
-for (var in dependent_vars) {
-  # Fit the full model
-  formula_full <- as.formula(paste(var, "~", independent_vars_full))
-  model_full <- lm(formula_full, data = hipp)
-  
-  # Extract coefficients and p-values for race2
-  race_coeff <- summary(model_full)$coefficients[grep("race2", rownames(summary(model_full)$coefficients)), ]
-  coeffs <- race_coeff[, "Estimate"]
-  p_vals <- round(race_coeff[, "Pr(>|t|)"], 4)
-  
-  # Perform ANOVA
-  formula_reduced <- as.formula(paste(var, "~", independent_vars_reduced))
-  model_reduced <- lm(formula_reduced, data = hipp)
-  global_p <- anova(model_reduced, model_full)[2, "Pr(>F)"]
-  
-  # Collect the global p-value and coefficient p-values
-  global_p_values <- c(global_p_values, global_p)
-  coeff_p_values <- c(coeff_p_values, p_vals)
-  
-  # Add results for each race comparison
-  for (i in 1:length(coeffs)) {
-    results <- rbind(results, data.frame(
-      names = paste(var, rownames(race_coeff)[i], sep = " "),
-      Coefficient = coeffs[i],
-      P.value = p_vals[i],
-      Adj.P.value = NA  # Placeholder for later
-    ))
-  }
-  
-  # Temporarily store global p-value (adjustment done later)
-  results <- rbind(results, data.frame(
-    names = paste(var, "Global p-value"),
-    Coefficient = NA,
-    P.value = global_p,
-    Adj.P.value = NA  # Placeholder
-  ))
-}
-
-# Adjust the global p-values together using FDR method
-global_p_values_adj <- p.adjust(global_p_values, method = "fdr")
-
-# Adjust the coefficient p-values together using FDR method
-coeff_p_values_adj <- p.adjust(coeff_p_values, method = "fdr")
-
-# Update adjusted p-values in the results data frame
-# Update adjusted p-values for coefficients
-coeff_index <- which(!grepl("Global p-value", results$names))
-results$Adj.P.value[coeff_index] <- coeff_p_values_adj
-
-# Update adjusted p-values for global p-values
-global_index <- grep("Global p-value", results$names)
-results$Adj.P.value[global_index] <- global_p_values_adj
-
-results
