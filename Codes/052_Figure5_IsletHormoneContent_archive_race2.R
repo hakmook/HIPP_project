a8e67bffb1b501638c5eab888bfcffee0721a9b9
xkcococo <xkcoco95@gmail.com>
2025-04-21 14:39:33 -0500

    archive version using race2 as one of covaraites; updated models using PC1 + PC2 + PC3 + PC4 + PC5 as covariates instead of race2

--- Codes/052_Figure5_IsletHormoneContent_archive_race2.R
+++ Codes/052_Figure5_IsletHormoneContent_archive_race2.R
@@ -9,11 +9,6 @@ library(dplyr)
 load("/Users/kexu/Library/CloudStorage/OneDrive-VUMC/Research/Active/20241022_HIPP/DATA1/data_process_meta.RData")
 hipp <- hipp
 
-load("/Users/kexu/Library/CloudStorage/OneDrive-VUMC/Research/Active/20241022_HIPP/DATA1/data_process_gen_dat.RData")
-gen_dat = gen_dat
-
-hipp$DONOR_RRID = substring(hipp$RRID,6)
-dat_all = merge(gen_dat, hipp, by="DONOR_RRID")
 
 ###################################################################
 # 3 main explanatory variables
@@ -29,14 +24,14 @@ GCG_vars <- c("GCG_basal_pg_IEQ", "GCG_G_16_7_AUC_pg_IEQ", "GCG_G_16_7_II",
               "GCG_G_1_7_Epi_1_AUC_pg_IEQ", "GCG_G_1_7_Epi_1_SI", "GCG_KCl_20_AUC_pg_IEQ", 
               "GCG_KCl_20_SI")
 
-co_vars <- c("Donor_HbA1c_s", "Gender", "PC1", "PC2", "PC3", "PC4", "PC5", "Age_years_s", "center", "BMI_s", "PreShipmentCultureTime", "IsletTransitTime")
+co_vars <- c("Donor_HbA1c_s", "Gender", "race2", "Age_years_s", "center", "BMI_s", "PreShipmentCultureTime", "IsletTransitTime")
 ###################################################################
 ###################################################################
 vars_int = vars_int
 outcome_int_ins = INS_vars
 outcome_int_glu = GCG_vars
 
-hipp <- dat_all[, c(vars_int, INS_vars, GCG_vars, co_vars)]
+hipp <- hipp[, c(vars_int, INS_vars, GCG_vars, co_vars)]
 
 ############################################
 ### Insulin secretion and Islet_Insulin_Content_ng_IEQ_s
@@ -46,7 +41,7 @@ Summary_Table = vector(mode='list', length=2)
 insulin_tb = NULL
 for (m in 1:length(outcome_int_ins)){
   model = as.formula(paste0(outcome_int_ins[m], "~", 
-                            "Islet_Insulin_Content_ng_IEQ_s", " + Gender + Donor_HbA1c_s + PC1 + PC2 + PC3 + PC4 + PC5 + Age_years_s + center + BMI_s + 
+                            "Islet_Insulin_Content_ng_IEQ_s", " + Gender + Donor_HbA1c_s + race2 + Age_years_s + center + BMI_s + 
                PreShipmentCultureTime + IsletTransitTime"))
   fit = lm(model, data = hipp)
   temp = summary(fit)
@@ -72,7 +67,7 @@ Summary_Table = vector(mode='list', length=2)
 insulin_tb = NULL
 for (m in 1:length(outcome_int_glu)){
   model = as.formula(paste0(outcome_int_glu[m], "~", 
-                            "Islet_Insulin_Content_ng_IEQ_s", " + Gender + Donor_HbA1c_s + PC1 + PC2 + PC3 + PC4 + PC5 + Age_years_s + center + BMI_s + 
+                            "Islet_Insulin_Content_ng_IEQ_s", " + Gender + Donor_HbA1c_s + race2 + Age_years_s + center + BMI_s + 
                PreShipmentCultureTime + IsletTransitTime"))
   fit = lm(model, data = hipp)
   temp = summary(fit)
@@ -98,7 +93,7 @@ Summary_Table = vector(mode='list', length=2)
 insulin_tb = NULL
 for (m in 1:length(outcome_int_ins)){
   model = as.formula(paste0(outcome_int_ins[m], "~", 
-                            "Islet_Glucagon_Content_pg_IEQ_s", " + Gender + Donor_HbA1c_s + PC1 + PC2 + PC3 + PC4 + PC5 + Age_years_s + center + BMI_s + 
+                            "Islet_Glucagon_Content_pg_IEQ_s", " + Gender + Donor_HbA1c_s + race2 + Age_years_s + center + BMI_s + 
                PreShipmentCultureTime + IsletTransitTime"))
   fit = lm(model, data = hipp)
   temp = summary(fit)
@@ -124,7 +119,7 @@ Summary_Table = vector(mode='list', length=2)
 insulin_tb = NULL
 for (m in 1:length(outcome_int_glu)){
   model = as.formula(paste0(outcome_int_glu[m], "~", 
-                            "Islet_Glucagon_Content_pg_IEQ_s", " + Gender + Donor_HbA1c_s + PC1 + PC2 + PC3 + PC4 + PC5 + Age_years_s + center + BMI_s + 
+                            "Islet_Glucagon_Content_pg_IEQ_s", " + Gender + Donor_HbA1c_s + race2 + Age_years_s + center + BMI_s + 
                PreShipmentCultureTime + IsletTransitTime"))
   fit = lm(model, data = hipp)
   temp = summary(fit)
